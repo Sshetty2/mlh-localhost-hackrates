c5d6b91797fe5e1005867685d9b02d354a38a4aa
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-01 13:02:35 -0400

    update truffle config.js
    the truffle config.js file determines how the contract will be migrated to the network. In this case, we need to specify a specific solidity compiler just so that we don't run into any wierd issues when compiling older contracts, and we need to instantiate a new Wallet with HDWalletProvider given our user-specific mnemonic and RPC endpoint. The mnemonic is given when we create a metamask wallet and the RPC enpoint is taken from the ethereum on azure output log from our master node.

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,18 +1,29 @@
 /*
  * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
  * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
  */
+require('dotenv').config();
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var rpc_endpoint = process.env.RPC_ENDPOINT || "http://ethirgqw4-dns-reg1.eastus.cloudapp.azure.com:8540";
+var mnemonic = process.env.MNEMONIC || "oval copper access play kidney pact drop club proof foil census photo"
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    // development: {
+    //   host: "localhost",
+    //   port: 8545,
+    //   network_id: "*" // Match any network id
+    // },
+    poa: {
+      provider: new HDWalletProvider(mnemonic, rpc_endpoint),
+      network_id: '10101010',
+      gasPrice : 0 // gas is not required in POA
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.23",
+    }
+  }
 };
